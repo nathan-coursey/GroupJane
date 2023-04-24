7a57ada97877ef33d216b456ef9c10e230523426
Jami <jamidefenbaugh@gmail.com>
2023-04-23 19:21:11 -0500

    added login for events page

--- Rally-FrontEnd/src/app/app-routing.module.ts
+++ Rally-FrontEnd/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginUserComponent } from './user-profile-arm/login-register/login-user/login-user.component';
 import { RegisterUserComponent } from './user-profile-arm/login-register/register-user/register-user.component';
 import { UserProfileComponent } from './user-profile-arm/user-profile/user-profile/user-profile.component';
+import { EventViewComponent } from './event-view/event-view.component';
+
 
 const routes: Routes = [
   {path: 'login', component: LoginUserComponent},
   {path: 'register', component: RegisterUserComponent},
-  {path: 'myProfile', component: UserProfileComponent}
+  {path: 'myProfile', component: UserProfileComponent},
+  {path: 'events', component: EventViewComponent}
+
 ];
 
 @NgModule({
