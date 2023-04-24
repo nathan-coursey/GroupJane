b97be54f4ded8c1dae9cf0047749a5820cce7b3b
AJmints <alexjmints@gmail.com>
2023-04-24 18:03:26 -0500

    UserProfile Image, route  updates

--- Rally-FrontEnd/src/app/app-routing.module.ts
+++ Rally-FrontEnd/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginUserComponent } from './user-profile-arm/login-register/login-user/login-user.component';
 import { RegisterUserComponent } from './user-profile-arm/login-register/register-user/register-user.component';
 import { UserProfileComponent } from './user-profile-arm/user-profile/user-profile/user-profile.component';
+import { DropdownMenuComponent } from './user-profile-arm/experiments/dropdown-menu/dropdown-menu.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginUserComponent},
   {path: 'register', component: RegisterUserComponent},
-  {path: 'myProfile', component: UserProfileComponent}
+  {path: 'myProfile', component: UserProfileComponent},
+  {path: 'testing', component: DropdownMenuComponent}
 ];
 
 @NgModule({
