dc6c6e6da997dee00cb1cd46b1b8ed6179702920
Jami <jamidefenbaugh@gmail.com>
2023-04-27 15:20:22 -0500

    fixed errors, added html/css

--- Rally-FrontEnd/src/app/event-create/event-create.component.ts
+++ Rally-FrontEnd/src/app/event-create/event-create.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-create',
