bc671da657c6185f3c2108ed945dd75112701678
Jami <jamidefenbaugh@gmail.com>
2023-04-29 18:00:07 -0500

    added code to process create event form

--- Rally-BackEnd/backend/src/main/java/org/rally/backend/eventsarm/controller/EventController.java
+++ Rally-BackEnd/backend/src/main/java/org/rally/backend/eventsarm/controller/EventController.java
@@ -1,7 +1,11 @@
 package org.rally.backend.eventsarm.controller;
 
+import org.rally.backend.eventsarm.models.Event;
 import org.rally.backend.eventsarm.repository.EventRepository;
+import org.rally.backend.userprofilearm.model.response.AuthenticationSuccess;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,32 +17,31 @@ public class EventController {
     @Autowired
     private EventRepository eventRepository;
 
-    @GetMapping("/events")
-    public String displayAllEvents() {
+//    @GetMapping("/events")
+//    public ResponseEntity<?> displayAllEvents(@RequestBody Event event) {
+//        return new ResponseEntity<>()
+////        return "/events";
+//    }
 
-        return "/events";
-    }
+//    @GetMapping("/event")
+//    public String displayEventPage() {
+//
+//        return "/event";
+//    }
 
-    @GetMapping("/event")
-    public String displayEventPage() {
-
-        return "/event";
-    }
-
-    @GetMapping("/create")
-    public String displayCreateEventForm() {
-
-
-        return "/create";
-    }
 
     @PostMapping("/create")
-    public String processCreateEventForm() {
-
-        return "/events";
+    public ResponseEntity<?> createEventForm(@RequestBody Event event) {
+        eventRepository.save(event);
+        AuthenticationSuccess authenticationSuccess = new AuthenticationSuccess("Success");
+        return new ResponseEntity<>(authenticationSuccess, HttpStatus.OK);
     }
 
-
+    //ResponseEntity represents the whole HTTP response from front end
+    //@RequestBody pulls from client side and puts data into Java object (event)
+    //new event saved into event repository
+    //authenticate user
+    //return new event from authenticated user
 
 
 }
