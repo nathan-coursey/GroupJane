39a418288754b721d1477386251469c78014b649
Jami <jamidefenbaugh@gmail.com>
2023-04-29 10:55:23 -0500

    added Event class

--- Rally-BackEnd/backend/src/main/java/org/rally/backend/eventsarm/models/Event.java
+++ Rally-BackEnd/backend/src/main/java/org/rally/backend/eventsarm/models/Event.java
@@ -12,10 +12,64 @@ public class Event extends AbstractEntity {
 
     private String eventAddress;
 
-    private String eventCategory;
+    private EventCategory eventCategory;
 
+    public Event(String eventHost, String contactEmail, String eventTitle, String eventAddress, EventCategory eventCategory) {
+        this.eventHost = eventHost;
+        this.contactEmail = contactEmail;
+        this.eventTitle = eventTitle;
+        this.eventAddress = eventAddress;
+        this.eventCategory = eventCategory;
+    }
 
+    public String getEventHost() {
+        return eventHost;
+    }
 
+    public void setEventHost(String eventHost) {
+        this.eventHost = eventHost;
+    }
 
+    public String getContactEmail() {
+        return contactEmail;
+    }
 
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getEventTitle() {
+        return eventTitle;
+    }
+
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle = eventTitle;
+    }
+
+    public String getEventAddress() {
+        return eventAddress;
+    }
+
+    public void setEventAddress(String eventAddress) {
+        this.eventAddress = eventAddress;
+    }
+
+    public EventCategory getEventCategory() {
+        return eventCategory;
+    }
+
+    public void setEventCategory(EventCategory eventCategory) {
+        this.eventCategory = eventCategory;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "eventHost='" + eventHost + '\'' +
+                ", contactEmail='" + contactEmail + '\'' +
+                ", eventTitle='" + eventTitle + '\'' +
+                ", eventAddress='" + eventAddress + '\'' +
+                ", eventCategory=" + eventCategory +
+                '}';
+    }
 }
