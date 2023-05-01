c5ddc0200a9a73e5142035c8693b73b44c14549a
Jami <jamidefenbaugh@gmail.com>
2023-05-01 09:26:44 -0500

    added EventCategory class

--- Rally-FrontEnd/src/app/Events/models/event.ts
+++ Rally-FrontEnd/src/app/Events/models/event.ts
@@ -1,3 +1,5 @@
+import { EventCategory } from "./eventCategory";
+
 export class Event {
     id: number;
     eventHost: string;
@@ -5,10 +7,10 @@ export class Event {
     eventTitle: string;   
     datetime: string;
     eventAddress: string;
-    eventCategory: string;
+    eventCategory: EventCategory[];
     description: string;
 
-    constructor(id: number, eventHost: string, contactEmail: string, eventTitle: string, datetime: string, eventAddress: string, eventCategory: string, description: string) {
+    constructor(id: number, eventHost: string, contactEmail: string, eventTitle: string, datetime: string, eventAddress: string, eventCategory: EventCategory[], description: string) {
         this.id = id;
         this.eventHost = eventHost;
         this.contactEmail = contactEmail;
@@ -17,6 +19,17 @@ export class Event {
         this.eventAddress = eventAddress;
         this.eventCategory = eventCategory;
         this.description = description;
+    }
+
+    getFormattedEventCategories(): string {
+        let eventCategories = "";
+        for(let i=0; i < this.eventCategory.length; i++) {
+            eventCategories += this.eventCategory[i].eventCategory;
+            if(i < this.eventCategory.length -1) {
+                eventCategories +=", ";
+            }
+        }
+        return eventCategories;
     }
 
 }
---
+++ Rally-FrontEnd/src/app/Events/models/eventCategory.ts
@@ -0,0 +1,10 @@
+export class EventCategory {
+
+    id: number;
+    eventCategory: string;
+
+    constructor(id: number, eventCategory: string) {
+        this.id = id;
+        this.eventCategory = eventCategory;
+    }
+}
